fa027168dffb71cd1d9eddebb0cb64636aaebdc1
Kshitij_Chandrakar <kshitijchandrakar2019@gmail.com>
2025-01-01 22:04:41 +0530

    Pushing-content

---
+++ static/redirectScriptColor.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('click', function(event) {
+    if (event.target.tagName === 'A') {
+      console.log('Link clicked:', event.target.href);
+    }
+  });
+});
+
+window.onload = function() {
+  var randomElement = "custom_color" + (Math.floor(Math.random() * 5) + 1);
+  console.log('Page loaded!');
+  document.querySelector('body').classList.remove("custom_color2");
+  document.querySelector('body').classList.add(randomElement);
+};
